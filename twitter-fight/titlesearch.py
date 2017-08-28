5e2a4bc3857f3bbf269476debbd666bb23d14ad8
Charles Arnaudo <charles.arnaudo@monotype.com>
2017-08-28 16:16:13 -0400

    title search

--- twitter-fight/titlesearch.py
+++ twitter-fight/titlesearch.py
@@ -1,3 +1,52 @@
 from TwitterSearch import *
 
-try
+try:
+    tuo = TwitterUserOrder('Chazapoo')
+
+    ts = TwitterSearch(
+        consumer_key = '',
+        consumer_secret = '',
+        access_token = '',
+        access_token_secret = ''
+    )
+
+    tf = 0
+    sum = 0
+    for tweet in ts.search_tweets_iterable(tuo):
+        #print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+        sum += 1
+        if "title" in tweet['text'].lower():
+            tf += 1
+        if "fight" in tweet['text'].lower():
+            tf += 1
+        if "title fight" in tweet['text'].lower():
+            tf += 1
+        if "27" in tweet['text'].lower():
+            tf += 1
+        if "numb" in tweet['text'].lower():
+            tf += 1
+        if "shed" in tweet['text'].lower():
+            tf += 1
+        if "floral" in tweet['text'].lower():
+            tf += 1
+        if "society" in tweet['text'].lower():
+            tf += 1
+        if "skin" in tweet['text'].lower():
+            tf += 1
+        if "hyperview" in tweet['text'].lower():
+            tf += 1
+        if "ceiling fan" in tweet['text'].lower():
+            tf += 1
+        if "all away" in tweet['text'].lower():
+            tf += 1
+        if "kingston" in tweet['text'].lower():
+            tf += 1
+        if "hopeless" in tweet['text'].lower():
+            tf += 1
+
+    avg = sum / tf
+    ans = float("{0:.2f}".format(avg))
+
+    print ("You have tweeted about Title Fight in approximately " + str(ans) + "% of your tweets")
+except TwitterSearchException as e: # catch all those ugly errors
+    print(e)
