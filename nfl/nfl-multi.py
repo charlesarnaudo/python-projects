80b17dd83453c8540bb8eece0cf4f65b383451aa
Charles Arnaudo <arnaudoc@wob-arnaudoc-x12.local>
2017-08-28 14:05:26 -0400

    Movin

--- nfl/nfl-multi.py
+++ nfl/nfl-multi.py
@@ -5,10 +5,19 @@ import nflgame
 class NFLFunc(object):
 
     def __init__(self, args):
-        self.args = args
+        self.year = args.y
+        self.week = args.w
+        self.limit = args.l
 
-    def test(self):
-        return "test"
+
+    def passer(self):
+        games = nflgame.games(2012, 5)
+        players = nflgame.combine_game_stats(games)
+
+        for p in players.passing().sort('passing_tds').limit(self.limit):
+            msg = '%s  %d completed, %d passed, for %d yards, %d TDs and %d INTs'
+            return msg % (p, p.passing_cmp, p.passing_att, p.passing_yds, p.passing_tds, p.passing_ints)
+
 
 class NFLParse(object):
 
@@ -33,13 +42,13 @@ Common commands:
     def passer(self):
         parser = argparse.ArgumentParser(
             description='Get information about passers')
-        parser.add_argument('-w', metavar='', help='week to seach for', type=int)
-        parser.add_argument('-y', metavar='', help='year to seach for', type=int)
-        parser.add_argument('-l', metavar='', default=5, help='limit of results', type=int)
+        parser.add_argument('-w', metavar='', help='week to seach for', type=int, required=True)
+        parser.add_argument('-y', metavar='', help='year to seach for', type=int, required=True)
+        parser.add_argument('-l', metavar='', default=5, help='limit of results', type=int, required=True)
         # 2 onwards for the rest of args
         args = parser.parse_args(sys.argv[2:])
         func = NFLFunc(args)
-        print func.test()
+        print func.passer()
 
 
 if __name__ == '__main__':
