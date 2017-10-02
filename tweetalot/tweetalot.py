f2fb3c76dda69759fbad7a780fee4e79ec583928
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-10-02 17:42:25 -0400

    pep8 fixes

--- tweetalot/tweetalot.py
+++ tweetalot/tweetalot.py
@@ -1,18 +1,21 @@
-import config, argparse, twitter
+import config
+import argparse 
+import twitter
 
 
 def __main__():
-    api = twitter.Api(consumer_key = config.consumer_key,
-                      consumer_secret = config.consumer_secret,
-                      access_token_key = config.access_token_key,
-                      access_token_secret = config.access_token_secret)
+    api=twitter.Api(consumer_key=config.consumer_key,
+                      consumer_secret=config.consumer_secret,
+                      access_token_key=config.access_token_key,
+                      access_token_secret=config.access_token_secret)
 
     parser = argparse.ArgumentParser(description='tweet a lot')
 
     parser.add_argument('-f',
-                        nargs = 1, type=str,
+                        nargs=1,
+                        type=str,
                         metavar='<input_file>',
-                        help = 'file which containts text to tweet',
-                        required = True)
+                        help='file which containts text to tweet',
+                        required=True)
 
-    args = parser.parse_args()
+    args=parser.parse_args()
--- twitter-fight/titlesearch.py
+++ twitter-fight/titlesearch.py
@@ -2,17 +2,18 @@ import argparse
 from TwitterSearch import *
 
 parser = argparse.ArgumentParser(description="Get username")
-parser.add_argument('-u', metavar='', help='user to seach against', type=str, required=True)
+parser.add_argument('-u', metavar='', help='user to seach against',
+                    type=str, required=True)
 
 args = parser.parse_args()
 
 tuo = TwitterUserOrder(args.u)
 
 ts = TwitterSearch(
-    consumer_key = config.consumer_key,
-    consumer_secret = config.consumer_secret,
-    access_token = config.access_token,
-    access_token_secret = config.access_token_secret
+    consumer_key=config.consumer_key,
+    consumer_secret=config.consumer_secret,
+    access_token=config.access_token,
+    access_token_secret=config.access_token_secret
 )
 
 tf = ['title',
@@ -47,8 +48,7 @@ tf = ['title',
       'chlorine',
       'don\'t count on me',
       'hypernight',
-      'rose of sharon'
-]
+      'rose of sharon']
 
 occ = 0
 count = 0
@@ -56,8 +56,9 @@ for tweet in ts.search_tweets_iterable(tuo):
     count += 1
     for term in tf:
         if term in tweet['text'].lower():
-            occ +=1
+            occ += 1
 
 ans = float("{0:.2f}".format(occ / count))
 
-print(args.u + " has tweeted about Title fight in " + str(ans) + "% of their tweets")
+print(args.u + " has tweeted about Title fight in " + str(ans) +
+               "% of their tweets")
